5cec351b153e9a6b301bc599b153808796bc9520
ArgentumWalker <svidchen-ko@mail.ru>
2017-09-28 23:38:53 +0300

    Project base

---
+++ standalone-game-client/src/main/java/ru/spbau/svidchenko/asteroids_project/standalone_game_client/Main.java
@@ -0,0 +1,5 @@
+package ru.spbau.svidchenko.asteroids_project.standalone_game_client;
+
+public class Main {
+    public static void main(String[] args) {}
+}
---
+++ training-arena/src/main/java/ru/spbau/svidchenko/asteroids_project/training_arena/Main.java
@@ -0,0 +1,5 @@
+package ru.spbau.svidchenko.asteroids_project.training_arena;
+
+public class Main {
+    public static void main(String[] args) {}
+}
